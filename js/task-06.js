75dde46cf0541458f6006c4b0de8f31553e459ab
OPaslavskiy <bankir.op@gmail.com>
2022-12-02 20:16:02 +0200

    task 06 ready

---
+++ js/task-06.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(`#validation-input`);
+console.log(input);
+
+input.addEventListener(`blur`, checkNumberOfSymbol);
+
+function checkNumberOfSymbol() {
+  if (input.value.length !== Number(input.dataset.length)) {
+    input.classList.add(`invalid`);
+  } else input.classList.replace(`invalid`, `valid`);
+}
