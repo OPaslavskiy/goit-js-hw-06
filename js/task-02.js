b7e1a5ebe9de8be02123ea85625a004b030027f1
OPaslavskiy <bankir.op@gmail.com>
2022-12-02 12:19:38 +0200

    task 02 Ready

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const itemArray = [];
+
+function creatItem() {
+  for (let i = 0; i < ingredients.length; i += 1) {
+    const li = document.createElement("li");
+    li.textContent = ingredients[i];
+    li.classList.add("item");
+    itemArray.push(li);
+  }
+}
+
+creatItem();
+
+list.append(...itemArray);
