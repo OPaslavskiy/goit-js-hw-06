1e855201bdc74b7474af25a69640a8972c2096b8
OPaslavskiy <bankir.op@gmail.com>
2022-12-02 22:18:19 +0200

    task 09 ready

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyOfDocument = document.querySelector("body");
+const styleOfDocument = document.querySelector("style");
+const spanColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+changeColorBtn.addEventListener("click", changeColorOfBody);
+
+let colorForBody = "";
+
+function changeColorOfBody(event) {
+  colorForBody = `body{background-color: ${getRandomHexColor()};}`;
+
+  styleOfDocument.insertAdjacentHTML("beforeend", colorForBody);
+  spanColor.textContent = getRandomHexColor();
+}
+
+console.log(styleOfDocument);
