d61a8ce3396bb99e77c6c5ee3507742a9be9d201
OPaslavskiy <bankir.op@gmail.com>
2022-12-02 19:40:29 +0200

    task 05

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,10 @@
 const userName = document.querySelector(`#name-output`);
 const input = document.querySelector(`#name-input`);
 
-input.addEventListener(`input`, () => (userName.textContent = input.value));
+input.addEventListener(`input`, changeName);
+
+function changeName() {
+  if (input.value) {
+    userName.textContent = input.value;
+  } else userName.textContent = "Anonymous";
+}
