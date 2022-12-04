7eaefca931ae97e77299688b56e4c4a6e0ce8b4f
OPaslavskiy <bankir.op@gmail.com>
2022-12-04 15:30:30 +0200

    to fix 07 task

--- js/task-07.js
+++ js/task-07.js
@@ -3,9 +3,6 @@ const article = document.querySelector(`#text`);
 
 input.addEventListener("input", changeSizeOfArticle);
 
-let sizeOfArticle = "";
-console.dir(article.style);
-
 function changeSizeOfArticle() {
   article.style.fontSize = `${input.value}px`;
 }
