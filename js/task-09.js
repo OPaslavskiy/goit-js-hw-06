9c05a2c8ff7cdd2c08b5e9fffe964403f9d36297
OPaslavskiy <bankir.op@gmail.com>
2022-12-04 15:20:15 +0200

    to fix 09 task

--- js/task-09.js
+++ js/task-09.js
@@ -2,19 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const bodyOfDocument = document.querySelector("body");
-const styleOfDocument = document.querySelector("style");
+const bodyOfDocument = document.body;
 const spanColor = document.querySelector(".color");
 const changeColorBtn = document.querySelector(".change-color");
 changeColorBtn.addEventListener("click", changeColorOfBody);
 
-let colorForBody = "";
+// let colorForBody = "";
 
 function changeColorOfBody() {
-  colorForBody = `body{background-color: ${getRandomHexColor()};}`;
+  const colorForBody = getRandomHexColor();
 
-  styleOfDocument.insertAdjacentHTML("beforeend", colorForBody);
-  spanColor.textContent = getRandomHexColor();
+  bodyOfDocument.style.backgroundColor = colorForBody;
+  spanColor.textContent = colorForBody;
 }
-
-console.log(styleOfDocument);
